8dfa18d7b168fcf60024e36c92b07ad93bb8a200
mr-a-100125 <gamidiajay@gmail.com>
2021-07-24 20:16:42 +0530

    Result of a*b+c.

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -6,6 +6,6 @@ read -p "Enter value for -> a : " a
 read -p "Enter value for -> b : " b
 read -p "Enter value for -> c : " c
 
-#result1 of a+b*c
-result1=$(($a+$b*$c))
-echo "$result1"
+#result2 of a*b+c
+result2=$(($a*$b+$c))
+echo "$result2"
