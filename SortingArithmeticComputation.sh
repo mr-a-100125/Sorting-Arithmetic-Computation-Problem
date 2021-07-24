ab5185a4dcd9c8905a1b3797fbc571262087f6db
mr-a-100125 <gamidiajay@gmail.com>
2021-07-24 20:39:10 +0530

    Merging UC1, UC2, UC3, UC4 to master

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -6,6 +6,18 @@ read -p "Enter value for -> a : " a
 read -p "Enter value for -> b : " b
 read -p "Enter value for -> c : " c
 
+#result1 of a+b*c
+result1=$(($a+$b*$c))
+echo "$result1"
+
+#result2 of a*b+c
+result2=$(($a*$b+$c))
+echo "$result2"
+
 #result3 of c*a/b
 result3=$(($c+$a/$b))
 echo "$result3"
+
+#result4 of a%b+c
+result4=$(($a%$b+$c))
+echo "$result4"
