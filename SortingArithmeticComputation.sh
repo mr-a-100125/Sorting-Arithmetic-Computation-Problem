ea9cc8b370a827e2c86c6564871114756a8c4de1
mr-a-100125 <gamidiajay@gmail.com>
2021-07-24 20:31:36 +0530

    Result of a%b+c.

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -6,6 +6,6 @@ read -p "Enter value for -> a : " a
 read -p "Enter value for -> b : " b
 read -p "Enter value for -> c : " c
 
-#result3 of c*a/b
-result3=$(($c+$a/$b))
-echo "$result3"
+#result4 of a%b+c
+result4=$(($a%$b+$c))
+echo "$result4"
