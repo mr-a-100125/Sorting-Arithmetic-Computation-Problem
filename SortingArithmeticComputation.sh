10e0a156e915aa684bc7d0f608c65eb056a8b05d
mr-a-100125 <gamidiajay@gmail.com>
2021-07-24 20:08:59 +0530

    Raeding a,b, & c values from user.

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -1,2 +1,7 @@
 #!/bin/bash 
 echo "Welcome to Sorting Arithmetic Computation Problem."
+
+#reading a,b &c values.
+read -p "Enter value for -> a : " a
+read -p "Enter value for -> b : " b
+read -p "Enter value for -> c : " c
