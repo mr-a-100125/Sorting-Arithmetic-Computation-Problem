9dbb0b61badcb12023583509fb5dead0ce75562b
mr-a-100125 <gamidiajay@gmail.com>
2021-07-24 20:44:07 +0530

    Storing arithmetic result into a dictonary.

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -6,18 +6,20 @@ read -p "Enter value for -> a : " a
 read -p "Enter value for -> b : " b
 read -p "Enter value for -> c : " c
 
+declare -A resultDict
+
 #result1 of a+b*c
 result1=$(($a+$b*$c))
-echo "$result1"
+resultDict[a+b*c]=$result1
 
 #result2 of a*b+c
 result2=$(($a*$b+$c))
-echo "$result2"
+resultDict[a*b+c]=$result2
 
 #result3 of c*a/b
 result3=$(($c+$a/$b))
-echo "$result3"
+resultDict[c*a/b]=$result3
 
 #result4 of a%b+c
 result4=$(($a%$b+$c))
-echo "$result4"
+resultDict[a%b+c]=$result4
